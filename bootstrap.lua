36398cbb140d027226641fbc114c9fb30e72d7b3
RandomSHelly67 <shellycursed948@gmail.com>
2026-01-16 14:51:30 +0100

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -23,6 +23,34 @@ end
 
 local State = requireModule("State")
 local CharacterService = requireModule("CharacterService")
--- later: OutfitService, UI
+local OutfitService = requireModule("OutfitService")
 
-print("[Bootstrap] Finished successfully")
+-- Save current character as "MyOutfit"
+OutfitService.SaveOutfit("MyOutfit", {
+    Head = CharacterService.Head,
+    Torso = CharacterService.Torso,
+    Face = CharacterService.Face,
+    FaceTextureId = CharacterService.FaceTextureId,
+    Shirt = CharacterService.Shirt,
+    ShirtTemplateId = CharacterService.ShirtTemplateId,
+    Pants = CharacterService.Pants,
+    PantsTemplateId = CharacterService.PantsTemplateId
+})
+
+-- Load an outfit
+local outfit = OutfitService.LoadOutfit("MyOutfit")
+if outfit then
+    CharacterService.Head = outfit.Head or {}
+    CharacterService.Torso = outfit.Torso or {}
+    CharacterService.Face = outfit.Face
+    CharacterService.FaceTextureId = outfit.FaceTextureId
+    CharacterService.Shirt = outfit.Shirt
+    CharacterService.ShirtTemplateId = outfit.ShirtTemplateId
+    CharacterService.Pants = outfit.Pants
+    CharacterService.PantsTemplateId = outfit.PantsTemplateId
+
+    -- Apply immediately
+    if game.Players.LocalPlayer.Character then
+        CharacterService.OnCharacterAdded(game.Players.LocalPlayer.Character)
+    end
+end
