3ae066144f86e0ade6d321ba5d9fc911a21c740a
RandomSHelly67 <shellycursed948@gmail.com>
2026-01-16 14:28:46 +0100

    Create State.lua

---
+++ State.lua
@@ -0,0 +1,18 @@
+-- State.lua
+local State = {}
+
+State.Time = 0.1
+
+State.Head = {}
+State.Torso = {}
+
+State.Face = nil
+State.FaceTextureId = nil
+
+State.Shirt = nil
+State.ShirtTemplateId = nil
+
+State.Pants = nil
+State.PantsTemplateId = nil
+
+return State
