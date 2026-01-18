9767aa47e9f9f130fd2d82b94f33e4ab958d24bf
RandomSHelly67 <shellycursed948@gmail.com>
2026-01-18 15:31:49 +0100

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -48,7 +48,7 @@ local function requireModule(name)
     return result
 end
 
-log("Starting bootstrap... | v2.5")
+log("Starting bootstrap... | v2.5 test")
 
 -- Load modules
 local CharacterService = requireModule("CharacterService")
