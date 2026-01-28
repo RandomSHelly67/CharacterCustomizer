7d2d8a0ad9705e5c1ce0f6c67b6905435968bb9e
RandomSHelly67 <shellycursed948@gmail.com>
2026-01-28 01:15:29 +0100

    Update bootstrap.lua

--- bootstrap.lua
+++ bootstrap.lua
@@ -48,7 +48,7 @@ local function requireModule(name)
     return result
 end
 
-log("Starting bootstrap... | v2.6-7 Outfit Copier")
+log("Starting bootstrap... | v2.6-7 Outfit Copier2")
 
 -- Load modules
 local CharacterService = requireModule("CharacterService")
